66bf7b219548c063b20c7d605d669ed2a96495f8
Neutron-Cntrl <hugo.lindfors2@elev.ga.ntig.se>
2022-09-13 11:38:57 +0200

    c

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,47 +1,47 @@
 <script setup> // lang="js"
-    import { ref } from 'vue';
-    import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
-    import { useRouter } from 'vue-router'; // import router from vue-router
-    const email = ref("");
-    const password = ref("");
-    const errMsg = ref(); // ERROR MESSAGE 
-    const router = useRouter() // get a reference to our vue router
-    const auth = getAuth(); // from firebase/auth
-    const register = () => { // register-method logs in user with email and password
-        // need .value because ref()
-        signInWithEmailAndPassword(getAuth(), email.value, password.value)
-            .then((data) => {
-                console.log("Successfully registered new account");
-    
-                console.log(auth.currentUser)
-    
-                router.push('/feed') // redirect to the feed-page
-            })
-            .catch((error) => {
-                console.log(`ERROR: ${error.code}`);
-                switch (error.code) { // show ERROR MESSAGE
-                    case "auth/invalid-email":
-                        errMsg.value = "Invalid email";
-                        break;
-                    case "auth/user-not-found":
-                        errMsg.value = "No account with that email was found";
-                        break;
-                    case "auth/wrong-password":
-                        errMsg.value = "Incorrect password";
-                        break; 
-                    case "auth/user-disabled":
-                        errMsg.value = "User has beeen permanently disabled";
-                        break;
-                    default:
-                        errMsg.value = "Email or password was incorrect";
-                        break;
-                }
-            })
-    };
-    const signInWithGoogle = () => {
-    
-    };
-    </script>
+import { ref } from 'vue';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+import { useRouter } from 'vue-router'; // import router from vue-router
+const email = ref("");
+const password = ref("");
+const errMsg = ref(); // ERROR MESSAGE 
+const router = useRouter() // get a reference to our vue router
+const auth = getAuth(); // from firebase/auth
+const register = () => { // register-method logs in user with email and password
+    // need .value because ref()
+    signInWithEmailAndPassword(getAuth(), email.value, password.value)
+        .then((data) => {
+            console.log("Successfully registered new account");
+
+            console.log(auth.currentUser)
+
+            router.push('/feed') // redirect to the feed-page
+        })
+        .catch((error) => {
+            console.log(`ERROR: ${error.code}`);
+            switch (error.code) { // show ERROR MESSAGE
+                case "auth/invalid-email":
+                    errMsg.value = "Invalid email";
+                    break;
+                case "auth/user-not-found":
+                    errMsg.value = "No account with that email was found";
+                    break;
+                case "auth/wrong-password":
+                    errMsg.value = "Incorrect password";
+                    break; 
+                case "auth/user-disabled":
+                    errMsg.value = "User has beeen permanently disabled";
+                    break;
+                default:
+                    errMsg.value = "Email or password was incorrect";
+                    break;
+            }
+        })
+};
+const signInWithGoogle = () => {
+
+};
+</script>
     
     <template>
         <div class="register">
