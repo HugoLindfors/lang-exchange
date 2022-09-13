9c6ea8d3a4b6bdc97ef6541c3a11fb2c8282dc3d
Neutron-Cntrl <hugo.lindfors2@elev.ga.ntig.se>
2022-09-13 14:19:47 +0200

    c

---
+++ src/components/List.vue
@@ -0,0 +1,17 @@
+<script setup> //lang="js"
+import { store } from './store';
+</script>
+
+<template>
+    
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            store
+        }
+    },
+}
+</script>
--- src/views/Feed.vue
+++ src/views/Feed.vue
@@ -1,11 +1,13 @@
 <script setup> //lang="js"
 import Search from '../components/Search.vue';
 import Filter from '../components/Filter.vue';
+import List from '../components/List.vue';
 </script>
 
 <template>
     <div class="feed">
         <Search></Search>
         <Filter></Filter>
+        <List></List>
     </div>
 </template>
